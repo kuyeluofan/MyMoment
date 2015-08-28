4692a40dc7a28dc7d3d56bfad737b5fddbea41a2
叶帆 <yefan@yefandeMacBook-Pro.local>
2015-08-28 14:33:08 +0800

    界面布局、按钮、倒计时

--- MyMoment/MainViewController.m
+++ MyMoment/MainViewController.m
@@ -9,7 +9,12 @@
 #import "MainViewController.h"
 
 @interface MainViewController ()
-@property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (weak, nonatomic) IBOutlet UIButton *switchButton;
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *todayMinuteNumberLabel;
+@property (weak, nonatomic, getter = startTimer)  NSTimer *timer;
+@property  (nonatomic) NSUInteger countTimeSecondNumber;
+@property (nonatomic) NSUInteger tomatoTimeSecondNumber;
 
 @end
 
@@ -18,7 +23,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor colorWithRed:255.0/255 green:255.0/255 blue:255.0/255 alpha:1];
-    self.startButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.switchButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.tomatoTimeSecondNumber = 1500;
     // Do any additional setup after loading the view.
 }
 
@@ -39,6 +45,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)switchButtonClick:(id)sender {
+    if ([self.switchButton.currentTitle isEqualToString:@"Start"]) {
+        //点击开始倒计时
+        
+        self.countTimeSecondNumber = self.tomatoTimeSecondNumber;
+        [self startTimer];
+        
+        [self.switchButton setTitle:@"Cancel" forState:UIControlStateNormal];
+    } else {
+        //点击取消计时
+        [self.timer invalidate];
+        
+        self.countTimeSecondNumber = self.tomatoTimeSecondNumber;
+        [self.switchButton setTitle:@"Start" forState:UIControlStateNormal];
+    }
+}
+
 /*
 #pragma mark - Navigation
 
@@ -52,4 +75,52 @@
 - (UIStatusBarStyle)preferredStatusBarStyle{
     return UIStatusBarStyleLightContent;
 }
+
+- (void)countDown{
+    
+    if (self.countTimeSecondNumber == 0) {
+        [self.timer invalidate];
+        
+        return;
+    }
+    self.countTimeSecondNumber -= 1;
+  
+    
+}
+
+- (NSString *)countDownStringFromSeconds:(NSInteger)originSeconds{
+    NSInteger minutes = 0;
+    NSInteger seconds = 0;
+    if (originSeconds > 0) {
+        minutes = originSeconds / 60;
+        seconds = originSeconds % 60;
+    }
+    
+    NSString *minutesAndSecondsString = [NSString stringWithFormat:@"%ld:%02ld", minutes, seconds];
+    return minutesAndSecondsString;
+}
+
+
+#pragma mark - getter and setter
+
+- (NSTimer *)startTimer{
+    
+    if (!_timer) {
+        _timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(countDown) userInfo:nil repeats:YES];
+    }
+    return _timer;
+    
+}
+
+- (void)setCountTimeSecondNumber:(NSUInteger)countTimeSecondNumber{
+    
+    _countTimeSecondNumber = countTimeSecondNumber;
+    self.timerLabel.text = [self countDownStringFromSeconds:_countTimeSecondNumber];
+    
+}
+
+
+
+
+
 @end
